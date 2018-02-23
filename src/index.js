e32405f70cc0804ac4c12af0d41460b314c47398
TMcCool <tjmccool1@icloud.com>
2018-02-22 20:31:22 -0600

    completed npm exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // index js //
-"use strict";
+//not needed with npm //"use strict";
 
 
-
-
+//this will break //$(document).ready(function(){
 
 const $ = require('jquery');
-const sayHello = () => { console.log("Hello"); }
+const hello = require('./say-hello.js');
+
+const sayHello = () => {console.log("Hello");}
 sayHello();
 
-$('body').append("<h1>Hello</h1>").css('background-color', 'green');
+$('body').append("<h1>Hello</h1>")
+        .append("<h4>I got this!!!!!!</h4>")
+        .css({'background-color': 'green',
+                'text-align': 'center'
+            })
+        .append('<h2>' + hello.niceMessage() +'</h2>')
+        .append('<h2>' + hello.niceMessage2() +'</h2>');
+
+//});
+
---
+++ src/say-hello.js
@@ -0,0 +1,8 @@
+const niceMessage = ()  => {
+    return ('This is a nice message for the lesson!');
+};
+
+const niceMessage2 = () => {
+    return ('Hello, here is another nice message for the lesson!')};
+
+module.exports = {niceMessage, niceMessage2};
